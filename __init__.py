14ca324220337891ca7a9b7e1da84968ac9e2a38
abennai <159786935+abennai@users.noreply.github.com>
2024-02-16 17:59:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route('/histogramme/')
-def monisto():
+def histogramme():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
